ebd62ae06f4db887575455840cc85fdb0a8b6536
hidiem <hidiem@uninorte.edu.co>
2021-09-23 12:43:23 -0500

    tiburon por perro

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,6 +1,5 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados() {
-        
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
